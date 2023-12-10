0a29b2d75287518b7b56a3273ad0c5d5a1f01391
Dominique Rwema Bagirishya <52289151+rwema3@users.noreply.github.com>
2023-12-10 23:53:43 +0200

    headers

--- lib/data/services/api_service.dart
+++ lib/data/services/api_service.dart
@@ -64,3 +64,4 @@ class ApiService {
   static Future<List<TestData>> deleteTestData({String id}) async {
     final res = await http.delete(
       Uri.parse("https://api2021.herokuapp.com/delete/$id"),
+      headers: {
