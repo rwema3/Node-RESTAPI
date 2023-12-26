25ca76639449a87969b21e300aafd5ca929a2c31
Dominique Rwema Bagirishya <52289151+rwema3@users.noreply.github.com>
2023-12-26 23:56:58 +0200

    Update elevatedButton.dart

--- lib/widget/elevatedButton.dart
+++ lib/widget/elevatedButton.dart
@@ -1,14 +1,22 @@
+// Importing the material.dart library from the Flutter framework
 import 'package:flutter/material.dart';
 
+// Defining a custom widget named elevatedbutton
 Widget elevatedbutton(
   BuildContext context, {
+  // Optional parameters for the onPressed function and textString
   Function onPressed,
   String textString,
 }) {
+  // Returning an ElevatedButton widget
   return ElevatedButton(
+    // Assigning the onPressed function to the ElevatedButton
     onPressed: onPressed,
+    // Displaying the text on the ElevatedButton
     child: Text(textString),
+    // Applying a custom style to the ElevatedButton
     style: ElevatedButton.styleFrom(
+      // Setting the background color of the ElevatedButton to red
       primary: Colors.red,
     ),
   );
