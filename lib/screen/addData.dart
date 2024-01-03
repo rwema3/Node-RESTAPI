4c27280d76baaf3e209753e8dd0b3046636c6444
Dominique Rwema Bagirishya <52289151+rwema3@users.noreply.github.com>
2024-01-03 23:56:40 +0200

    Add data class

--- lib/screen/addData.dart
+++ lib/screen/addData.dart
@@ -2,4 +2,8 @@ import 'package:api3/data/model/testData.dart';
 import 'package:api3/data/services/api_service.dart';
 import 'package:api3/utilities/textStyle.dart';
 import 'package:api3/widget/appbar.dart';
+import 'package:api3/widget/elevatedButton.dart';
+import 'package:flutter/material.dart';
 
+class AddData extends StatefulWidget {
+ 
